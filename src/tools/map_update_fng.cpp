24bb38ff39b55c088a4378a6b1e31d1e3695d8cd
Sylvain <35niavlys@gmail.com>
2020-12-13 16:54:15 +0100

    fix map_update_fng (#6)

--- src/tools/map_update_fng.cpp
+++ src/tools/map_update_fng.cpp
@@ -3,10 +3,13 @@
 #include <base/system.h>
 #include <engine/shared/datafile.h>
 #include <engine/storage.h>
+#include <game/gamecore.h>
 #include <game/mapitems.h>
 
 int main(int argc, const char **argv)
 {
+	dbg_logger_stdout();
+
 	IStorage *pStorage = CreateStorage("Teeworlds", IStorage::STORAGETYPE_BASIC, argc, argv);
 	int Index, ID = 0, Type = 0, Size;
 	void *pPtr;
@@ -15,14 +18,26 @@ int main(int argc, const char **argv)
 	CDataFileWriter df;
 
 	if(!pStorage || argc != 3)
+	{
+		dbg_msg("map_update_fng", "usage: %s input.map output.map", argv[0]);
 		return -1;
+	}
 
 	str_format(aFileName, sizeof(aFileName), "%s", argv[2]);
 
 	if(!DataFile.Open(pStorage, argv[1], IStorage::TYPE_ABSOLUTE))
+	{
+		dbg_msg("map_update_fng", "error opening input map '%s'", argv[1]);
 		return -1;
+	}
 	if(!df.Open(pStorage, aFileName, IStorage::TYPE_ABSOLUTE))
+	{
+		dbg_msg("map_update_fng", "error opening output map '%s'", aFileName);
 		return -1;
+	}
+
+	char aGame[16];
+	str_format(aGame, sizeof(aGame), "Game");
 
 	// add all items
 	for(Index = 0; Index < DataFile.NumItems(); Index++)
@@ -30,20 +45,26 @@ int main(int argc, const char **argv)
 		pPtr = DataFile.GetItem(Index, &Type, &ID);
 		Size = DataFile.GetItemSize(Index);
 
+		// update outdated index
 		if(Type == MAPITEMTYPE_LAYER)
 		{
 			CMapItemLayerTilemap *pTileMap = (CMapItemLayerTilemap *)pPtr;
-			CTile *pTiles = (CTile *)DataFile.GetData(pTileMap->m_Data);
+			char aName[16];
+			IntsToStr(pTileMap->m_aName, sizeof(pTileMap->m_aName) / sizeof(int), aName);
 
-			for(int y = 0; y < pTileMap->m_Height; y++)
+			if(str_comp(aGame, aName) == 0)
 			{
-				for(int x = 0; x < pTileMap->m_Width; x++)
+				CTile *pTiles = (CTile *)DataFile.GetData(pTileMap->m_Data);
+
+				for(int y = 0; y < pTileMap->m_Height; y++)
 				{
-					int Index = pTiles[y * pTileMap->m_Width + x].m_Index;
-					// uddate outdated index
-					if(Index >= 208 && Index <= 210)
+					for(int x = 0; x < pTileMap->m_Width; x++)
 					{
-						pTiles[y * pTileMap->m_Width + x].m_Index -= 200;
+						int Index = pTiles[y * pTileMap->m_Width + x].m_Index;
+						if(Index >= 208 && Index <= 210)
+						{
+							pTiles[y * pTileMap->m_Width + x].m_Index -= 200;
+						}
 					}
 				}
 			}
@@ -63,5 +84,6 @@ int main(int argc, const char **argv)
 	DataFile.Close();
 	df.Finish();
 
+	dbg_msg("map_update_fng", "Done");
 	return 0;
 }
